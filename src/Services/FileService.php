763059cc7d999563b0e94b6a6a6798d7b773abca
Draghici Denis Cosmin <154325453+DenisCosmin21@users.noreply.github.com>
2025-09-12 22:19:44 +0300

    Added the services needed to run

--- src/Services/FileService.php
+++ src/Services/FileService.php
@@ -4,5 +4,80 @@ namespace Deniscosmin21\LogServicePhp\Services;
 
 class FileService
 {
+    private static array $env = [];
 
+    public function findFile(string $fileName): ?string
+    {
+        $path = dirname(__DIR__, 2);
+
+        return $this->searchDirectory($path, $fileName);
+    }
+
+    public function searchDirectory(string $directory, string $fileName): ?string
+    {
+        $entries = scandir($directory);
+
+        foreach ($entries as $entry) {
+            if($entry == "." || $entry == "..")
+                continue;
+
+            if($entry == $fileName)
+                return realpath($directory . DIRECTORY_SEPARATOR . $entry);
+
+            if(is_dir($directory . DIRECTORY_SEPARATOR . $entry)){
+                $result = $this->searchDirectory($directory . DIRECTORY_SEPARATOR . $entry, $fileName);
+                if($result !== null){
+                    return $result;
+                }
+            }
+
+        }
+
+        return null;
+    }
+
+    public function readEnvFile(?string $filePath): array
+    {
+        $env = [];
+        if (!file_exists($filePath)) {
+            return $env;
+        }
+
+        $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($lines as $line) {
+            if (strpos(trim($line), '#') === 0) {
+                continue;
+            }
+
+            list($name, $value) = explode('=', $line, 2);
+            $env[trim($name)] = trim($value);
+        }
+
+        return $env;
+    }
+
+    public function getLogFile(string $type) : string
+    {
+        $mainPath = dirname(__DIR__, 2);
+
+        $fileName = self::getEnv('LOG_' . strtoupper($type) . '_PATH');
+
+        if($fileName == '')
+            $fileName = "log_records/all.log";
+
+        if(!is_dir($mainPath . DIRECTORY_SEPARATOR . 'log_records'))
+            mkdir($mainPath . DIRECTORY_SEPARATOR . 'log_records', 0755, true);
+
+        return $mainPath . DIRECTORY_SEPARATOR . $fileName;
+    }
+
+    public static function getEnv(string $key): string
+    {
+        return self::$env[$key] ?? '';
+    }
+
+    public static function setEnv(array $env): void
+    {
+        self::$env = $env;
+    }
 }
--- src/Services/RequestService.php
+++ src/Services/RequestService.php
@@ -2,7 +2,80 @@
 
 namespace Deniscosmin21\LogServicePhp\Services;
 
+use Deniscosmin21\LogServicePhp\Repositories\RequestRepository;
+
 class RequestService
 {
+    private RequestRepository $repository;
+    private FileService $fileService;
 
+    /**
+     * @param RequestRepository $repository
+     */
+    public function __construct(RequestRepository $repository, FileService $fileService)
+    {
+        $this->repository = $repository;
+        $this->fileService = $fileService;
+    }
+
+    public function sendLogs(array $logsCategories)
+    {
+        if(count($logsCategories) == 0)
+            return;
+
+        foreach($logsCategories as $key => $logs) {
+            if(count($logs) == 0)
+                continue;
+
+            if($key == 'main') {
+                 $resp = $this->repository->sendLogs($logs, FileService::getEnv('API_KEY'), FileService::getEnv('API_PASSWORD'));
+            }
+            else {
+                $password = $logs[0]['credentials']['value'];
+                $resp = $this->repository->sendLogs($logs, $key, $password);
+            }
+
+            if($resp['code'] != 200 || $resp['error'] != '' || isset($resp['response']['errors'])) {
+                $errors = [];
+
+                if($resp['error'] != '')
+                    $errors = $this->buildServerErrorResponse($resp['error'], count($logs));
+                else if(isset($resp['response']['errors']))
+                    $errors = $resp['response']['errors'];
+
+
+                $this->saveFailedLogs($logs, $errors);
+            }
+        }
+    }
+
+    private function saveFailedLogs(array $logs, array $errors)
+    {
+        foreach($errors as $idLog => $error) {
+            $message = $this->createLogMessage($logs[$idLog]);
+            $this->repository->saveFailedLog($message, $error, $this->fileService->getLogFile($logs[$idLog]['type']));
+        }
+    }
+
+    private function createLogMessage($log) : string
+    {
+        $date = date('Y-m-d');
+        $time = date('h:i:sa');
+
+        $message = '[' . strtoupper($log['type']) . ' log, in date : ' . $date . ' ' . $time . ']';
+        $message = $message . ' : Detalii : ' . $log['details'] . ' Locatie : ' . $log['location'];
+
+        return $message;
+    }
+
+    private function buildServerErrorResponse(string $error, int $count) : array
+    {
+        $errors = [];
+
+        for($i = 0; $i < $count; $i++) {
+            $errors[$i] = $error;
+        }
+
+        return $errors;
+    }
 }
