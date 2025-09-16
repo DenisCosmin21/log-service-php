c18f5b252553a492e9e8180c8e0c09830acbe069
Draghici Denis Cosmin <154325453+DenisCosmin21@users.noreply.github.com>
2025-09-16 14:43:14 +0300

    Changed save logs endpoint

--- src/Repositories/RequestRepository.php
+++ src/Repositories/RequestRepository.php
@@ -8,7 +8,7 @@ class RequestRepository
 
     public function __construct()
     {
-        $this->curl = curl_init("http://logservice-env-1.eba-wtnqyrgy.eu-central-1.elasticbeanstalk.com/api/save_log");
+        $this->curl = curl_init("http://logservice-env-1.eba-wtnqyrgy.eu-central-1.elasticbeanstalk.com/api/save_logs");
         curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($this->curl, CURLOPT_POST, true);
         curl_setopt($this->curl, CURLOPT_HTTPHEADER, [
